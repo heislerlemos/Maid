e91acc2db8541464695f319943481e0b42cfb829
heisler lemos <heisler.stlano@gmail.com>
2024-08-15 12:05:39 +0100

    Adicionado o metodo Adicionar

---
+++ Maid/AddMaid.cs
@@ -0,0 +1,14 @@
+﻿namespace Maid
+{
+	public class AddMaid
+	{
+		public int Maidid { get; set; }
+		public string? Nome { get; set; }
+		public string Address { get; set; } = null!;
+		public int Reputation { get; set; }
+		public string Telefone { get; set; } = null!;
+		public string Email { get; set; } = null!;
+
+
+	}
+}
