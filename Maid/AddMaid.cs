494065bfb4b3c31997248fc72911dca0a58cfc0f
heisler lemos <heisler.stlano@gmail.com>
2024-08-15 13:58:52 +0100

    Adicionando os campos de criação

--- Maid/AddMaid.cs
+++ Maid/AddMaid.cs
@@ -5,7 +5,7 @@
 		public int Maidid { get; set; }
 		public string? Nome { get; set; }
 		public string Address { get; set; } = null!;
-		public int Reputation { get; set; }
+		public int Idade { get; set; }
 		public string Telefone { get; set; } = null!;
 		public string Email { get; set; } = null!;
 
